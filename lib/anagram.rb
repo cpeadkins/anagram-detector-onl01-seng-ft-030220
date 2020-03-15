83cfcedfbb3b6fb89784f0b02d48f09e8a696ef4
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 17:18:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(array)
+    new_array = []
+    
+    array.each do |anagram|
+      if anagram.split("").sort == @word.split("").sort
+        new_array << anagram
+      end
+    end
+    
+    array_new
+  
+  end
+
+end
